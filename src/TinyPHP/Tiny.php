9d2dbfcf587f94e2088ed040aa948136b85998cd
Kakawait <thibaud.lepretre@gmail.com>
2014-02-28 14:31:41 +0100

    variable naming convention change

--- src/TinyPHP/Tiny.php
+++ src/TinyPHP/Tiny.php
@@ -19,69 +19,64 @@ class Tiny
         '$this'
     );
 
-    function isLabel($str)
+    private static function isLabel($str)
     {
         return preg_match('~[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+~', $str);
     }
 
-    function getTiny(
-        $snippet,
-        $replace_variables = false,
-        $remove_whitespace = false,
-        $remove_comments = false,
-        $additional_excludes = array()
+    public static function getTiny($src, $replaceVariables = false, $whitespaces = false, $comments = false,
+                                   $excludes = array()
     )
     {
         //create excluded variables array
-        $php_excludes = self::$EXCLUDES;
-        if (!empty($additional_excludes)) {
-            $excludes = array_merge($php_excludes, $additional_excludes);
+        if (!empty($excludes)) {
+            $excludes = array_merge(self::$EXCLUDES, $excludes);
             $excludes = array_unique($excludes);
         } else {
-            $excludes = $php_excludes;
+            $excludes = self::$EXCLUDES;
         }
 
         //generate tokens from snippet
-        $tokens = token_get_all($snippet);
+        $tokens = token_get_all($src);
 
         //get all variables in snippet, remove comments
         $variables = array();
-        $new_source = '';
+        $result = '';
         foreach ($tokens as $token) {
             if (!is_array($token)) {
-                $new_source .= $token;
+                $result .= $token;
                 continue;
             }
             if ($token[0] == T_VARIABLE && !in_array($token[1], $excludes)) {
                 $variables[] = $token[1];
             }
-            if ($remove_comments) {
+            if (!$comments) {
                 if (in_array($token[0], array(T_COMMENT, T_DOC_COMMENT))) {
                     continue;
                 }
             }
-            $new_source .= $token[1];
+            $result .= $token[1];
         }
         $variables = array_unique($variables);
 
         // generate replace values for all variables
-        $variable_replace = array();
+        $variableReplacement = array();
         if (!empty($variables)) {
             $count = 0;
             while ($variable = current($variables)) {
                 $mod = $count % 26;
 
-                $repeat_chars = ((int)floor($count / 26)) + 1;
+                $repeatChars = ((int)floor($count / 26)) + 1;
 
-                $replace_int = 97 + $mod;
-                $replace_value = chr(36);
+                $replaceInt = 97 + $mod;
+                $value = chr(36);
 
-                for ($i = 1; $i <= $repeat_chars; $i++) {
-                    $replace_value .= chr($replace_int);
+                for ($i = 1; $i <= $repeatChars; $i++) {
+                    $value .= chr($replaceInt);
                 }
 
-                if (!in_array($replace_value, $variables) && !in_array($replace_value, $excludes)) {
-                    $variable_replace[$variable] = $replace_value;
+                if (!in_array($value, $variables) && !in_array($value, $excludes)) {
+                    $variableReplacement[$variable] = $value;
                     next($variables);
                 }
                 $count++;
@@ -89,37 +84,37 @@ class Tiny
         }
 
         //get all tokens from previous iteration
-        $tokens = token_get_all($new_source);
+        $tokens = token_get_all($result);
 
         //replace all variables, remove whitespace
-        $new_source = '';
+        $result = '';
         foreach ($tokens as $i => $token) {
             if (!is_array($token)) {
-                $new_source .= $token;
+                $result .= $token;
                 continue;
             }
-            if ($replace_variables) {
+            if ($replaceVariables) {
                 if ($token[0] == T_VARIABLE && !in_array($token[1], $excludes)) {
                     $key = $token[1];
-                    $new_source .= $variable_replace[$key];
+                    $result .= $variableReplacement[$key];
                     continue;
                 }
             }
-            if ($token[0] == T_WHITESPACE && $remove_whitespace) {
+            if ($token[0] == T_WHITESPACE && !$whitespaces) {
                 if (isset($tokens[$i - 1]) && isset($tokens[$i + 1]) && is_array($tokens[$i - 1])
                     && is_array($tokens[$i + 1])
-                    && $this->isLabel($tokens[$i - 1][1])
-                    && $this->isLabel($tokens[$i + 1][1])
+                    && self::isLabel($tokens[$i - 1][1])
+                    && self::isLabel($tokens[$i + 1][1])
                 ) {
-                    $new_source .= ' ';
+                    $result .= ' ';
                 }
             } else if ($token[0] == T_CASE) {
-                $new_source .= $token[1] . ' ';
+                $result .= $token[1] . ' ';
             } else {
-                $new_source .= $token[1];
+                $result .= $token[1];
             }
         }
 
-        return $new_source;
+        return $result;
     }
 }
